10baf15d59a0b4de3619a83eb58f6a24636c0086
hanyluv <hanyluv.928@gmail.com>
2019-03-25 01:06:18 +0900

    공부한 코드 설명 주석 추가

--- app/src/main/java/com/example/mycatapp/CatsRepository.kt
+++ app/src/main/java/com/example/mycatapp/CatsRepository.kt
@@ -3,7 +3,7 @@ package com.example.mycatapp
 import io.reactivex.Single
 import io.reactivex.functions.Function
 
-class CatsRepository(
+class CatsRepository (
     baseUrl: String,
     isDebugEnabled: Boolean,
     apiKey: String
@@ -22,7 +22,8 @@ class CatsRepository(
 
     fun getNumberOfRandomCats(limit: Int, category_ids: Int?): Single<Result> {
         return catsDataSource
-            .getNumberOfRandomCats(limit, category_ids) // 반환값  Single<List<NetCat>>
+            .getNumberOfRandomCats(limit, category_ids)
+            // 반환값  Single<List<NetCat>>
 //            .map { netCats: List<NetCat> -> Result(netCats = netCats) }  // Single<List<NetCat>>.map 이거라고 보면댐.
             .map(object : Function<List<NetCat>,Result> {
                 //map. 결론적으로 api요청으로 받아온 값을, 우리가 쉽게 쓸수있는 클래스로 매핑해준다 생각하자. 여기선 Result 이다.
--- app/src/main/java/com/example/mycatapp/MainActivity.kt
+++ app/src/main/java/com/example/mycatapp/MainActivity.kt
@@ -7,6 +7,7 @@ import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
+import io.reactivex.subjects.ReplaySubject
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -30,13 +31,21 @@ class MainActivity : AppCompatActivity() {
     private fun getSomeCats() {
         val catsRepository = CatsRepository(serverUrl, BuildConfig.DEBUG, apiKey)
 
-        latestCatCall?.dispose()
+        latestCatCall?.dispose() //이게 만약에 실행중이면 스톱하는거야.
+        //Schedulers.io() - 동기 I/O를 별도로 처리시켜 비동기 효율을 얻기 위한 스케줄러.
+        //자체적인 스레드 풀 CachedThreadPool을 사용합니다. API 호출 등 네트워크를 사용한 호출 시 사용됩니다.
+        
+        //subscribeOn 는 Observable이 동작하는 스케쥴러를 다른 스케쥴러로 지정하여 동작을 변경함.
+        //Reactive-Stream에서 사용할 기본 스케줄러이며, observeOn으로 스케줄러 교체하기 전까지 기본으로 사용한다.
         latestCatCall = catsRepository.getNumberOfRandomCats(10, null).subscribeOn(Schedulers.io())
                 .doOnSubscribe {
+                    //RxJava를 사용할때 스트림을 취소해 주지 않으면 계속 돌기때문에 위험하다.
+                    //이를 해결하기위해 ArrayStream에 만들어진 코드를 넣어두고 한번에 취소하는 방법을 사용해야한다.
                     compositeDisposableOnPause.add(it)
                 }
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe { result ->
+                .observeOn(AndroidSchedulers.mainThread()) //결과를 받을 Thread.
+            //RxJava는 편의를 위해 사용하지 않는 구성이 누락된 인터페이스를 제공한다.
+                .subscribe { result -> //구독 받은거
                     when {
                         result.hasError() -> result.errorMessage?.let {
                             Toast.makeText(this@MainActivity, "Error getting cats$it", Toast.LENGTH_SHORT).show()
